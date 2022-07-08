70fddc2a8cb7af531b7e06facd4da2e2d480f12f
Renan Fachin <82840836+RenanFachin@users.noreply.github.com>
2022-07-08 00:26:04 -0300

    Add files via upload
    Adicionado Responsividade a página

--- script.js
+++ script.js
@@ -14,4 +14,14 @@ function changeText(color){
     textTwo.style.background = color;
 }
 
+function toggleMenu(){
+    var menuToggle = document.querySelector('.toggle');
+    var navigation = document.querySelector('.navigation')
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
 
+function changeSideBar(color){
+    const sideBar = document.querySelector('.sci');
+    sideBar.style.background = color;
+}
